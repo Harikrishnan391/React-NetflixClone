2ec9748cbf3cfac61736c86054644fd8e3b2dcc5
Harikrishnan <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>
2023-10-31 15:51:18 +0530

    Netflix react

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -38,7 +38,7 @@ function Banner() {
           <button className="button">Play</button>
           <button className="button">My List</button>
         </div>
-        <h1 className="description">{movie ? movie.overview : ""}</h1>
+        <h1 className="description">{movie && movie.overview }</h1>
       </div>
       <div className="fade_bottom"></div>
     </div>
